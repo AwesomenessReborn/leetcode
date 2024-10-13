fae95ec0f82f6ffe7422158a9af977a294df5147
AwesomenessReborn <hareee234@gmail.com>
2024-10-12 17:31:58 -0700

    374 Guess Number

---
+++ cpp_practice/374 Guess Number Higher or Lower/main.cpp
@@ -0,0 +1,87 @@
+// https://leetcode.com/problems/guess-number-higher-or-lower/?envType=study-plan-v2&envId=leetcode-75
+
+#include <bitset>
+#include <map>
+#include <math.h>
+#include <algorithm>
+#include <iostream>
+#include <array>
+#include <iterator> 
+#include <vector>
+#include <ios>
+#include <limits>
+#include <sstream> 
+#include <string> 
+
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+
+using namespace std; 
+
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+template <typename T>
+void printArray(std::vector<T> values) {
+    std::cout << "["; 
+    for (int i = 0; i < values.size(); i++) {
+        std::cout << values[i]; 
+        if (values.size() - 1 > i) {
+            std::cout << ", "; 
+        }
+    }
+    std::cout << "]" << std::endl; 
+}
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+int guess(int num) {
+    // TODO requires implementation. 
+    return 0; 
+}; 
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 1, high = n;
+        
+        while (low <= high) {
+            // Avoid overflow with this formula
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+            
+            if (res == 0) {
+                // We've found the correct number
+                return mid;
+            } else if (res == -1) {
+                // The guessed number is too high
+                high = mid - 1;
+            } else {
+                // The guessed number is too low
+                low = mid + 1;
+            }
+        }
+        
+        return -1; // This should never be reached
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
