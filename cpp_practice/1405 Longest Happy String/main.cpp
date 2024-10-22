2670dee4efedeae49d71d93e2c639449c9fc19c1
AwesomenessReborn <hareee234@gmail.com>
2024-10-21 22:19:14 -0700

    new solution

--- cpp_practice/1405 Longest Happy String/main.cpp
+++ cpp_practice/1405 Longest Happy String/main.cpp
@@ -6,12 +6,12 @@
 #include <algorithm>
 #include <iostream>
 #include <array>
-#include <iterator> 
+#include <iterator>
 #include <vector>
 #include <ios>
 #include <limits>
-#include <sstream> 
-#include <string> 
+#include <sstream>
+#include <string>
 
 #include <set>
 #include <unordered_set>
@@ -19,9 +19,10 @@
 #include <unordered_map>
 #include <queue>
 
-using namespace std; 
+using namespace std;
 
-static auto _ = [](){
+static auto _ = []()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
@@ -29,69 +30,93 @@ static auto _ = [](){
 }();
 
 template <typename T>
-void printArray(std::vector<T> values) {
-    std::cout << "["; 
-    for (int i = 0; i < values.size(); i++) {
-        std::cout << values[i]; 
-        if (values.size() - 1 > i) {
-            std::cout << ", "; 
+void printArray(std::vector<T> values)
+{
+    std::cout << "[";
+    for (int i = 0; i < values.size(); i++)
+    {
+        std::cout << values[i];
+        if (values.size() - 1 > i)
+        {
+            std::cout << ", ";
         }
     }
-    std::cout << "]" << std::endl; 
+    std::cout << "]" << std::endl;
 }
 
 /**
- * make sure you get as close to the length of a+b+c. 
- * 
- * 
- *          always make sure to use the most frequent character every time. 
- * 
- *          maintain que to track what's the first, second, and third highest letter. 
+ * make sure you get as close to the length of a+b+c.
+ *
+ *
+ *          always make sure to use the most frequent character every time.
+ *
+ *          maintain que to track what's the first, second, and third highest letter.
  */
 
-struct CompFrequency {
-    bool operator()(const std::pair<int, char>& a, const std::pair<int, char>& b) {
-        return a.first > b.first;  
+struct CompFrequency
+{
+    bool operator()(const std::pair<int, char> &a, const std::pair<int, char> &b)
+    {
+        return a.first > b.first;
     }
 };
 
-class Solution {
+class Solution
+{
 public:
-    string longestDiverseString(int a, int b, int c) {
-        string res; 
+    string longestDiverseString(int a, int b, int c)
+    {
+        priority_queue<pair<int, char>> pq;
+        if (a > 0)
+            pq.push({a, 'a'});
+        if (b > 0)
+            pq.push({b, 'b'});
+        if (c > 0)
+            pq.push({c, 'c'});
 
-        priority_queue<pair<int, char>, std::vector<std::pair<int, char>>, CompFrequency> que; 
-        if (a > 0) que.push({a, 'a'}); 
-        if (b > 0) que.push({b, 'b'}); 
-        if (c > 0) que.push({c, 'c'}); 
+        string result = "";
 
-        while (que.size() > 1) {
+        while (!pq.empty())
+        {
+            auto [count1, char1] = pq.top();
+            pq.pop();
 
-        }
+            if (result.size() >= 2 && result.back() == char1 && result[result.size() - 2] == char1)
+            {
+                if (pq.empty())
+                    break;
 
-        if (que.size() == 1) {
-            int fr = que.top().first; 
-            char ch = que.top().second; 
+                auto [count2, char2] = pq.top();
+                pq.pop();
 
-            if (fr <= 1) {
-                res += ch; 
-            } else {
-                res += ch; 
-                res += ch; 
+                result += char2;
+                count2--;
+
+                if (count2 > 0)
+                    pq.push({count2, char2});
+
+                pq.push({count1, char1});
             }
+            else
+            {
+                result += char1;
+                count1--;
 
+                if (count1 > 0)
+                    pq.push({count1, char1});
+            }
         }
 
-        return res; 
+        return result;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-    Solution sol; 
+    Solution sol;
 
-    cout << sol.longestDiverseString(1, 1, 7) << endl; 
-    cout << sol.longestDiverseString(7, 1, 0) << endl; 
+    cout << sol.longestDiverseString(1, 1, 7) << endl;
+    cout << sol.longestDiverseString(7, 1, 0) << endl;
 
     return 0;
 }
