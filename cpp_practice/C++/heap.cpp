bdbc4404aa18dd62895d84cae6d3a203ab86464a
AwesomenessReborn <hareee234@gmail.com>
2025-04-07 13:55:40 -0700

    heapify

--- cpp_practice/C++/heap.cpp
+++ cpp_practice/C++/heap.cpp
@@ -149,20 +149,32 @@ bool update(vector<int>& heap) {
     return false;
 }
 
-void buildHeap(vector<int>& heap) {
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
 }
 
 int main(int argc, char const *argv[])
 {
 
-    vector<int> values = {1, 5, 3, 2, 5}; 
+    vector<int> values = {1, 5, 3, 2, 4, 8, 10}; 
+    int n = values.size(); 
 
-    buildHeap(values); 
-
-    int output = extract(values); 
-
-    cout << output << endl; 
+    for (int i = n/2 - 1; i >= 0; i--) {
+        heapify(values, n, i); 
+    }
 
     printArray(values); 
 
